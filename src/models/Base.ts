55eef86a6ac17d9c686bda9ee48bef463e49b11a
Rohit Raj <rij7u7d@gmail.com>
2020-12-02 23:43:04 +0530

    Made some basic changes to the existing files

--- src/models/Base.ts
+++ src/models/Base.ts
@@ -1,14 +1,14 @@
 import { Schema, model, Model, Document } from 'mongoose';
 import { EMODELS } from './models.types';
 
+const BaseSchema = new Schema({
+    name: String,
+});
+
 export interface IBase {
     name: string;
 }
 
 export type IBaseModel = Model<IBase & Document>;
 
-const BaseSchema = new Schema({
-    name: String,
-});
-
-export const BaseModel: IBaseModel = model(EMODELS.BASE, new Schema(BaseSchema));
+export const BaseModel: IBaseModel = model(EMODELS.BASE, BaseSchema);
