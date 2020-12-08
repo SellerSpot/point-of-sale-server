62888f2b480fb6c2c8428004e592f7ccb7ca2571
Rohit Raj <rij7u7d@gmail.com>
2020-12-08 11:15:50 +0530

    Completed taxBracket APIs

--- src/controller/taxBracket.ts
+++ src/controller/taxBracket.ts
@@ -3,6 +3,8 @@ import { Connection } from 'mongoose';
 import { EMODELS } from '../models/models.types';
 import { TaxBracketModel } from '../models';
 import { IResponse } from '../typings/request.types';
+import Joi from 'joi';
+import { commonJoiSchemas, joiSchemaOptions } from '../utils';
 
 const getTaxBracketModel = (currentDb: Connection = global.currentDb): TaxBracketModel.ITaxBracketModel => {
     return currentDb.model(EMODELS.TAXBRACKET);
@@ -14,12 +16,14 @@ export const getTaxBrackets: RequestHandler = async (req: Request, res: Response
         const dbModel = getTaxBracketModel();
         response = {
             status: true,
+            statusCode: 'OK',
             data: await dbModel.find(),
         };
     } catch (e) {
         response = {
             status: false,
-            error: e.message,
+            statusCode: 'INTERNALSERVERERROR',
+            data: e.message,
         };
     } finally {
         res.send(response);
@@ -29,92 +33,89 @@ export const getTaxBrackets: RequestHandler = async (req: Request, res: Response
 export const createTaxBracket: RequestHandler = async (req: Request, res: Response) => {
     let response: IResponse;
     try {
-        const dbModel = getTaxBracketModel();
-        const taxBracketName = req.body['taxBracketName'];
-        // checking if TaxBracket already exists
-        if ((await dbModel.find({ name: taxBracketName })).length === 0) {
-            await dbModel.create({
-                name: taxBracketName,
-                taxPercent: req.body['taxBracketPercent'],
-            });
-            response = {
-                status: true,
-            };
-        } else {
+        const requestBodySchema = Joi.object({
+            name: Joi.string().alphanum().required(),
+            taxPercent: Joi.number().max(100).min(0).required(),
+        });
+        const { error, value } = requestBodySchema.validate(req.body, joiSchemaOptions);
+        req.body = value;
+        if (error) {
             response = {
                 status: false,
-                data: 'Tax Bracket already exists in database',
+                statusCode: 'BADREQUEST',
+                data: error.message,
             };
+        } else {
+            const dbModel = getTaxBracketModel();
+            const { name, taxPercent } = req.body;
+            // checking if TaxBracket already exists
+            if ((await dbModel.find({ name: name })).length === 0) {
+                await dbModel.create({
+                    name: name,
+                    taxPercent: taxPercent,
+                });
+                response = {
+                    status: true,
+                    statusCode: 'CREATED',
+                };
+            } else {
+                response = {
+                    status: false,
+                    statusCode: 'CONFLICT',
+                    data: 'Tax Bracket already exists in database',
+                };
+            }
         }
     } catch (e) {
         response = {
             status: false,
-            error: e.message,
+            statusCode: 'INTERNALSERVERERROR',
+            data: e.message,
         };
     } finally {
         res.send(response);
     }
 };
 
-// export const updateTaxBracket: RequestHandler = async (req: Request, res: Response) => {
-//     let response: IResponse;
-//     try {
-//         const dbModel = getTaxBracketModel();
-
-//         const taxBracketName = req.body['taxBracketName'];
-//         const newTaxBracketName = req.body['newTaxBracketName'];
-//         const newTaxBracketPercent = req.body['newTaxBracketPercent'];
-//         // checking if TaxBracket already exists
-
-//         if ((await dbModel.find({ name: taxBracketName })).length !== 0) {
-//             await dbModel.findOneAndUpdate(
-//                 {
-//                     name: taxBracketName,
-//                 },
-//                 { name: newTaxBracketName, taxPercent: newTaxBracketPercent },
-//             );
-//             response = {
-//                 status: true,
-//             };
-//         } else {
-//             response = {
-//                 status: false,
-//                 data: 'Tax Bracket does not exist in database',
-//             };
-//         }
-//     } catch (e) {
-//         response = {
-//             status: false,
-//             error: e.message,
-//         };
-//     } finally {
-//         res.send(response);
-//     }
-// };
-
 export const deleteTaxBracket: RequestHandler = async (req: Request, res: Response) => {
     let response: IResponse;
     try {
-        const dbModel = getTaxBracketModel();
-        const taxBracketName = req.body['taxBracketName'];
-        // checking if TaxBracket already exists
-        if ((await dbModel.find({ name: taxBracketName })).length !== 0) {
-            await dbModel.findOneAndDelete({
-                name: taxBracketName,
-            });
-            response = {
-                status: true,
-            };
-        } else {
+        const requestParamsSchema = Joi.object({
+            taxBracketid: commonJoiSchemas.MONGODBID.required(),
+        });
+        const { error, value } = requestParamsSchema.validate(req.params, joiSchemaOptions);
+        req.params = value;
+        if (error) {
             response = {
                 status: false,
-                data: 'Tax Bracket does not exist in database',
+                statusCode: 'BADREQUEST',
+                data: error.message,
             };
+        } else {
+            const dbModel = getTaxBracketModel();
+            const { taxBracketid } = req.params;
+            // checking if TaxBracket already exists
+            if ((await dbModel.findById({ taxBracketid })) !== null) {
+                await dbModel.findByIdAndDelete({
+                    taxBracketid,
+                });
+                response = {
+                    status: true,
+                    statusCode: 'NOCONTENT',
+                };
+            } else {
+                response = {
+                    status: false,
+                    statusCode: 'NOTFOUND',
+                    data: 'Tax Bracket does not exist in database',
+                };
+            }
         }
     } catch (e) {
         response = {
             status: false,
-            error: e.message,
+            statusCode: 'INTERNALSERVERERROR',
+            data: e.message,
         };
     } finally {
         res.send(response);
--- src/router/taxBracket.ts
+++ src/router/taxBracket.ts
@@ -6,6 +6,6 @@ const taxBracketRouter: Router = Router();
 taxBracketRouter.post('/', taxBracket.createTaxBracket);
 taxBracketRouter.get('/', taxBracket.getTaxBrackets);
 // taxBracketRouter.put('/', taxBracket.updateTaxBracket);
-taxBracketRouter.delete('/', taxBracket.deleteTaxBracket);
+taxBracketRouter.delete('/:taxBracketid', taxBracket.deleteTaxBracket);
 
 export default taxBracketRouter;
