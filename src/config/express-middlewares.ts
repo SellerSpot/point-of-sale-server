b9fa36b492de087dccd662ee7795a0ccacc542d1
Rohit Raj <rij7u7d@gmail.com>
2020-12-20 08:25:01 +0530

    Added cors

--- src/config/express-middlewares.ts
+++ src/config/express-middlewares.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { CONFIG } from '.';
 
 export const applyExpressMiddlewares = (app: express.Application): void => {
     app.use(express.json());
+    app.use(cors());
     app.use(morgan(CONFIG.ENV === 'development' ? 'dev' : 'short'));
 };
