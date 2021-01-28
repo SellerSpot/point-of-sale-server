f70cb0fed0000fc221c17f41d409bde2602d5980
Rohit Raj <rij7u7d@gmail.com>
2021-01-28 21:20:01 +0530

    Fix root router string interpolation

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,12 +12,12 @@ import { pointOfSaleTypes } from '@sellerspot/universal-types';
 const rootRouter: Router = Router();
 
 rootRouter.use(`/${pointOfSaleTypes.ROUTES.AUTHORIZE}`, authorizationRouter);
-rootRouter.use(pointOfSaleTypes.ROUTES.CATEGORY, categoryRouter);
+rootRouter.use(`/${pointOfSaleTypes.ROUTES.CATEGORY}`, categoryRouter);
 rootRouter.use(`/${pointOfSaleTypes.ROUTES.BRAND}`, brandRouter);
-rootRouter.use(pointOfSaleTypes.ROUTES.STOCK_UNIT, stockUnitRouter);
-rootRouter.use(pointOfSaleTypes.ROUTES.TAX_BRACKET, taxBracketRouter);
-rootRouter.use(pointOfSaleTypes.ROUTES.PROUDCT, productRouter);
-rootRouter.use(pointOfSaleTypes.ROUTES.SALE, saleRouter);
+rootRouter.use(`/${pointOfSaleTypes.ROUTES.STOCK_UNIT}`, stockUnitRouter);
+rootRouter.use(`/${pointOfSaleTypes.ROUTES.TAX_BRACKET}`, taxBracketRouter);
+rootRouter.use(`/${pointOfSaleTypes.ROUTES.PROUDCT}`, productRouter);
+rootRouter.use(`/${pointOfSaleTypes.ROUTES.SALE}`, saleRouter);
 rootRouter.use(homeRouter);
 
 export default rootRouter;
